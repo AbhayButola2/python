2dbc629afdc97fe591ac5c944473c92bdd2629f4
AbhayButola2 <149133544+AbhayButola2@users.noreply.github.com>
2024-04-13 10:39:52 +0530

    Create list2.py

---
+++ list2.py
@@ -0,0 +1,13 @@
+ist=['abhay', 'butola']
+list2=[500124835]
+list3=list+list2
+print(list3)
+
+if 'hi' in list3:
+    print("YES")
+else:
+    print("NO")
+if 'abhay' in list3:
+    print("abhay")
+else:
+    print("NO")
