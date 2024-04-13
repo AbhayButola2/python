7ba2eeb5b21901749c27df35f3da6cdaa173796c
AbhayButola2 <149133544+AbhayButola2@users.noreply.github.com>
2024-04-13 10:21:59 +0530

    Create func.py

---
+++ func.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+# EXAMPLE 1
+
+# convert list or tuple in numpy arrays
+l1 = [2,3,4,5,55,75,354,345,555]
+print(type(l1))
+
+# EXAMPLE 2
+
+#using arrange()function
+a3 = np.arange(0,20).reshape(4,5)
+print(a3)
