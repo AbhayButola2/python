e72eca889ae5871fd8f87d40f16bd8f5f75d02ee
AbhayButola2 <149133544+AbhayButola2@users.noreply.github.com>
2024-04-13 10:23:16 +0530

    Update func.py

--- func.py
+++ func.py
@@ -12,3 +12,16 @@ print(type(l1))
 #using arrange()function
 a3 = np.arange(0,20).reshape(4,5)
 print(a3)
+# EXAMPLE 3 
+
+# using random.randint
+#generates random integer ranges fron 0-100
+a=np.random.randint(0,100,20)
+# Reshape the array to a desired shape, for example, a 4x5 matrix
+reshaped_a = a.reshape(4, 5)
+# Display the original array and the reshaped array
+print("\nOriginal array:")
+print(a)
+print("\nReshaped array:")
+print(reshaped_a)
+print(reshaped_a[-1: , -1: ])
